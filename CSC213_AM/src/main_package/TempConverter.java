f0d25cad1bb2560e74e0169e911647d469cf8e8b
a7mor <a7mor@AMG32.lan1>
2023-09-11 20:04:09 -0700

    -Finished Project w/ no errors

--- CSC213_AM/src/main_package/TempConverter.java
+++ CSC213_AM/src/main_package/TempConverter.java
@@ -1,10 +1,35 @@
 package main_package;
+import java.util.Scanner;
 
 public class TempConverter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//OBJ: Write a program that will convert temperatures between Celsius and Fahrenheit.
+		Scanner userInput = new Scanner(System.in);
+		int numCelsius, numFahrenheit;
+		//Ask user what unit
+		System.out.print("Would you like to convert to Celsius or Fahrenheit(C/F)?: ");
+		String str = userInput.nextLine();
+		char choice = str.charAt(0);
+		//Ask for Temp value
+		System.out.print("Please enter temperature value: ");
+		int rawValue = userInput.nextInt();
+		//Check
+		switch(choice) 
+		{
+		case 'C':
+		case 'c':
+			//C to F
+			numFahrenheit = (rawValue * 9/5) + 32;
+			System.out.printf("The value in Fahrenheit is %d F", numFahrenheit);
+			break;
+		case 'F':
+		case 'f':
+			//F to C
+			numCelsius = (rawValue - 32) * 5/9;
+			System.out.printf("\nThe value in Celsius is %d C", numCelsius);
+			break;
+		default:
+		}
 	}
-
 }
