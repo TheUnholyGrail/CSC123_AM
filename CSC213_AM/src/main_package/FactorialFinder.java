ba8721103f1b690807f77b2fb6513d2849261d68
a7mor <a7mor@AMG32.lan1>
2023-09-12 10:47:50 -0700

    -Finished 4th project w/ no Errors

---
+++ CSC213_AM/src/main_package/FactorialFinder.java
@@ -0,0 +1,25 @@
+package main_package;
+import java.util.Scanner;
+public class FactorialFinder {
+	//Alejandro Morales (amorales298@toromail.csudh.edu)
+	public static void main(String[] args) {
+		//OBJ: Develop a program that computes the factorial of a number given through the command prompt.
+		Scanner userInput = new Scanner(System.in);
+		int rawValue;
+		//Check for valid input
+		do 
+		{
+			//Ask user to input a positive integer
+			System.out.print("Please input a positive integer: ");
+			rawValue = userInput.nextInt();
+			
+		}while(rawValue <= 0);
+		//Compute the factorial of the number
+		int factorial = 1;
+		for(int i = 1;i <= rawValue;i++) {
+			factorial *= i;
+		}
+		//Display the result
+		System.out.printf("The factorial of %d! is %d", rawValue, factorial);
+	}
+}
